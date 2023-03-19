de7d00e5fd123f6f6520a7cae1674e7d293e5551
muraleo <muraleo80923@gmail.com>
2023-03-19 18:32:15 -0400

    trigger new build

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from './components/MarketingApp';
 export default () => {
   return (
     <div>
-      <h1>Go Go Container!</h1>
+      <h1>Go Container!</h1>
       <hr />
       <MarketingApp />
     </div>
