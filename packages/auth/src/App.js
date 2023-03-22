ec112b3cab7497e58f45096b254fc250d7ed1f9b
muraleo <muraleo80923@gmail.com>
2023-03-21 20:25:16 -0400

    fix typo

--- packages/auth/src/App.js
+++ packages/auth/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Switch, Route, Router } from 'react-router-dom';
 import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
 
-import Signin from './components/SignIn';
+import Signin from './components/Signin';
 import Signup from './components/Signup';
 
 const generateClassName = createGenerateClassName({
