621767bd89cafc5625aae6e5375b93d97d0e71c9
Jake Abbott <jake.abbott@hey.com>
2023-03-24 10:06:25 -0400

    fix pitchboard webpack settings

--- packages/pitchboard/config/webpack.prod.js
+++ packages/pitchboard/config/webpack.prod.js
@@ -11,7 +11,7 @@ const prodConfig = {
   },
   plugins: [
     new ModuleFederationPlugin({
-      name: 'Pitchboard',
+      name: 'pitchboard',
       filename: 'remoteEntry.js',
       exposes: {
         './PitchboardApp': './src/bootstrap',
