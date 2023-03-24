73c88f46380811eee138421a5c399300c6704c4e
Diego Prestes <diego.prestes@alphasights.com>
2023-03-24 11:57:39 +0000

    adding form subapp

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,18 +1,19 @@
-import React, { lazy, Suspense, useState } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import React, { lazy, Suspense, useState } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import {
   StylesProvider,
   createGenerateClassName,
-} from '@material-ui/core/styles';
+} from "@material-ui/core/styles";
 
-import Progress from './components/Progress';
-import Header from './components/Header';
+import Progress from "./components/Progress";
+import Header from "./components/Header";
 
-const MarketingLazy = lazy(() => import('./components/MarketingApp'));
-const AuthLazy = lazy(() => import('./components/AuthApp'));
+const MarketingLazy = lazy(() => import("./components/MarketingApp"));
+const AuthLazy = lazy(() => import("./components/AuthApp"));
+const FormLazy = lazy(() => import("./components/FormApp"));
 
 const generateClassName = createGenerateClassName({
-  productionPrefix: 'co',
+  productionPrefix: "co",
 });
 
 export default () => {
@@ -31,6 +32,7 @@ export default () => {
               <Route path="/auth">
                 <AuthLazy onSignIn={() => setIsSignedIn(true)} />
               </Route>
+              <Route path="/form" component={FormLazy} />
               <Route path="/" component={MarketingLazy} />
             </Switch>
           </Suspense>
--- packages/container/src/components/Header.js
+++ packages/container/src/components/Header.js
@@ -1,59 +1,63 @@
-import React from 'react';
-import AppBar from '@material-ui/core/AppBar';
-import Button from '@material-ui/core/Button';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
-import { makeStyles } from '@material-ui/core/styles';
-import { Link as RouterLink } from 'react-router-dom';
+import React from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Button from "@material-ui/core/Button";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Box from "@material-ui/core/Box";
+import { makeStyles } from "@material-ui/core/styles";
+import { Link as RouterLink } from "react-router-dom";
 
 const navItems = [
   {
-    text: 'Home',
-    url: '/'
+    text: "Home",
+    url: "/",
   },
   {
-    text: 'Pricing',
-    url: '/pricing'
+    text: "Pricing",
+    url: "/pricing",
   },
   {
-    text: 'Link One',
-    url: '/link-one'
+    text: "Link One",
+    url: "/link-one",
   },
   {
-    text: 'Link Two',
-    url: '/link-two'
+    text: "Link Two",
+    url: "/link-two",
   },
   {
-    text: 'Link Three',
-    url: '/link-three'
-  }
+    text: "Link Three",
+    url: "/link-three",
+  },
+  {
+    text: "Form",
+    url: "/form",
+  },
 ];
 
 const useStyles = makeStyles((theme) => ({
-  '@global': {
+  "@global": {
     ul: {
       margin: 0,
       padding: 0,
-      listStyle: 'none',
+      listStyle: "none",
     },
     a: {
-      textDecoration: 'none',
+      textDecoration: "none",
     },
   },
   appBar: {
     borderBottom: `1px solid ${theme.palette.divider}`,
   },
   toolbar: {
-    flexWrap: 'wrap',
-    justifyContent: 'space-between',
+    flexWrap: "wrap",
+    justifyContent: "space-between",
   },
   linkBox: {
-    display: 'flex',
-    alignItems: 'center'
+    display: "flex",
+    alignItems: "center",
   },
   menu: {
-    marginLeft: '20px'
+    marginLeft: "20px",
   },
   link: {
     margin: theme.spacing(1, 1.5),
@@ -63,14 +67,14 @@ const useStyles = makeStyles((theme) => ({
   },
   cardHeader: {
     backgroundColor:
-      theme.palette.type === 'light'
+      theme.palette.type === "light"
         ? theme.palette.grey[200]
         : theme.palette.grey[700],
   },
   cardPricing: {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'baseline',
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "baseline",
     marginBottom: theme.spacing(2),
   },
   footer: {
@@ -78,7 +82,7 @@ const useStyles = makeStyles((theme) => ({
     marginTop: theme.spacing(8),
     paddingTop: theme.spacing(3),
     paddingBottom: theme.spacing(3),
-    [theme.breakpoints.up('sm')]: {
+    [theme.breakpoints.up("sm")]: {
       paddingTop: theme.spacing(6),
       paddingBottom: theme.spacing(6),
     },
@@ -115,11 +119,7 @@ export default function Header({ isSignedIn, onSignOut }) {
             </Typography>
             <Box className={classes.menu}>
               {navItems.map((item) => (
-                <Button
-                  component={RouterLink}
-                  to={item.url}
-                  key={item.url}
-                >
+                <Button component={RouterLink} to={item.url} key={item.url}>
                   {item.text}
                 </Button>
               ))}
@@ -130,10 +130,10 @@ export default function Header({ isSignedIn, onSignOut }) {
             variant="outlined"
             className={classes.link}
             component={RouterLink}
-            to={isSignedIn ? '/' : '/auth/signin'}
+            to={isSignedIn ? "/" : "/auth/signin"}
             onClick={onClick}
           >
-            {isSignedIn ? 'Logout' : 'Login'}
+            {isSignedIn ? "Logout" : "Login"}
           </Button>
         </Toolbar>
       </AppBar>
